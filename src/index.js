12f9d54056538886a8db7eac86577a6a88d6641e
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2021-08-14 20:54:55 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string} name - Require name
+ * @returns {boolean}
+ */
+function isloaded(name) {
+	try {
+		const structureName = require.resolve(name);
+		return Boolean(require.cache[structureName]);
+	} catch(err) {
+		return false;
+	}
+}
+
+/**
+ * @param {string} name - Require name
+ */
+function unload(name) {
+	try {
+		const structureName = require.resolve(name);
+		delete require.cache[structureName];
+		return true;
+	} catch(err) {
+		return false;
+	}
+}
+
+/**
+ * Patches require export
+ * @param {string} name - Require name
+ * @param {Function} patcher - Function, returning a new export
+ */
+function patch(name, patcher) {
+	if(!isloaded(name)) throw new Error(`Module "${name}" not found`);
+
+	const structure = require(name);
+	const structureName = require.resolve(name);
+	const patched = patcher(structure);
+	require.cache[structureName].exports = patched;
+	return patched;
+}
+
+module.exports = {
+	isloaded,
+	unload,
+	patch
+};
